227e0f2ac4d6af0c89546fc968527f662f8b45d1
Mohammed Foda <modyfoda404@gmail.com>
2025-11-30 22:27:40 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,21 @@
+async function FetchAPI(){
+    const productsCo = document.getElementById("products");
+    try{
+        const apifetch = await fetch("https://fakestoreapi.com/products");
+        const products = await apifetch.json();
+        products.forEach(product => {
+            productsCo.innerHTML = productsCo.innerHTML + 
+            `
+                <div class="product-card">
+                    <img src="${product.image}" alt="${product.title}" />
+                    <h3>${product.title}</h3>
+                    <p>${product.description.substring(0, 100)}</p>
+                    <div class="price">$${product.price}</div>
+                </div>
+            `;
+        });
+    }catch(error){
+        console.error("Error", error);
+    }
+}
+FetchAPI();
